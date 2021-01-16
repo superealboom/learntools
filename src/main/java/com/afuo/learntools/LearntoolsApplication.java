a8a015e716f1344694aa42366eff0d08b5970a07
tianci <superealboom@163.com>
2021-01-16 18:26:25 +0800

    第一次提交

---
+++ src/main/java/com/afuo/learntools/LearntoolsApplication.java
@@ -0,0 +1,13 @@
+package com.afuo.learntools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearntoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearntoolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/afuo/learntools/LearntoolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afuo.learntools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearntoolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
